c12ab8a2ee1ebbdf9d223222dd5dff8a5cfbb7d8
DTMichael <Zhuyi2632@163.com>
2020-02-06 22:42:39 +0800

    springboot整合springcloud,实现了生产者、消费者和注册中心的简单微服务系统

---
+++ eureka-server/src/main/java/com/atguigu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动注解
+ * 1、配置eureka
+ * 2、@EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
